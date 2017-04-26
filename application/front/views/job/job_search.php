5578cddc58a16a92c2708463858547c8f1cad064
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-04-26 11:43:42 +0530

    add search icon

--- application/front/views/job/job_search.php
+++ application/front/views/job/job_search.php
@@ -10,7 +10,12 @@
                               <select class="" name="searchplace[]" id="searchplace" multiple="multiple">
                                 </select>
                             </fieldset>
-                            <fieldset class="col-md-2">
+                        <!--     <fieldset class="col-md-2">
                                 <button onclick="return checkvalue();"> Search</button>
+                            </fieldset>
+                             -->
+                            <fieldset class="col-md-2">
+                                  <label for="search_btn" id="search_f"><i class="fa fa-search" aria-hidden="true"></i></label>
+                                <button onclick="return checkvalue();"  id="search_btn" style="display: none;"> Search</button>
                             </fieldset>
 <?php echo form_close();?>
