67dae36f4b9a3c797dc1f761b24f2f8a1e842909
vishang2aileensoul <vishang2aileensoul@gmail.com>
2017-04-27 18:42:21 +0530

    set message button in freelacerhire post, freelancerhire profile , freelancer post profile

--- application/front/views/freelancer/freelancer_hire/freelancer_hire_post.php
+++ application/front/views/freelancer/freelancer_hire/freelancer_hire_post.php
@@ -260,7 +260,7 @@ label.cameraButton input[accept*="camera"] {
         <div class="profile-main-rec-box-menu  col-md-12 ">
 
             <div class="left-side-menu col-md-2">  </div>
-            <div class="right-side-menu col-md-9 " style="padding-left: 0px;">  
+            <div class="right-side-menu col-md-8 " style="padding-left: 0px;">  
                 <ul class="">                          
                     <li <?php if (($this->uri->segment(1) == 'freelancer') && ($this->uri->segment(2) == 'freelancer_hire_profile')) { ?> class="active" <?php } ?>><a href="<?php echo base_url('freelancer/freelancer_hire_profile'); ?>">Employer Details</a>
                     </li>
@@ -286,6 +286,22 @@ label.cameraButton input[accept*="camera"] {
 
                 </ul>
             </div>
+    
+            <div class="col-md-2">
+                <div class="flw_msg_btn fr">
+                    <ul>
+                        <!-- <li class="fruser2">
+
+                                <div id="unfollowdiv">
+                                    <a id="unfollow2" onclick="unfollowuser(2)"> Following</a>
+                                </div>
+                        </li> -->
+                        <li>
+                            <a href="http://35.165.1.109:81/chat/abc/4">Message</a></li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
 
 
--- application/front/views/freelancer/freelancer_hire/freelancer_hire_profile.php
+++ application/front/views/freelancer/freelancer_hire/freelancer_hire_profile.php
@@ -158,7 +158,7 @@ label.cameraButton input[accept*="camera"] {
 <div class="profile-main-rec-box-menu  col-md-12 ">
 
 <div class="left-side-menu col-md-2">  </div>
-<div class="right-side-menu col-md-9">  
+<div class="right-side-menu col-md-8">  
  <ul class="">
                                      <li <?php if(($this->uri->segment(1) == 'freelancer') && ($this->uri->segment(2) == 'freelancer_hire_profile')){?> class="active" <?php } ?>><a href="<?php echo base_url('freelancer/freelancer_hire_profile'); ?>">Employer Details</a>
                                     </li>
@@ -184,6 +184,21 @@ label.cameraButton input[accept*="camera"] {
 
     </ul>
     </div>
+
+    <div class="col-md-2">
+                <div class="flw_msg_btn fr">
+                    <ul>
+                        <!-- <li class="fruser2">
+
+                                <div id="unfollowdiv">
+                                    <a id="unfollow2" onclick="unfollowuser(2)"> Following</a>
+                                </div>
+                        </li> -->
+                        <li>
+                            <a href="http://35.165.1.109:81/chat/abc/4">Message</a></li>
+                    </ul>
+                </div>
+            </div>
 
 </div>
                
