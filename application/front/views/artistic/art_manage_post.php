a4aa45dc0287ba1d45c7cf60d79fdcdd4732d7e5
DESKTOP-RJTC1MK\yatin <aileensoftsolution@gmail.com>
2017-04-29 01:04:17 +0530

    empty space link removed

--- application/front/views/artistic/art_manage_post.php
+++ application/front/views/artistic/art_manage_post.php
@@ -1202,7 +1202,7 @@ responsive image design start -->
                         <!-- other user post time name end-->
                         <?php }else{?>
                         <a style=" font-size: 18px;
-                         line-height: 24px; font-weight: 600; color: #000033; margin-bottom: 4px; "  href="<?php echo base_url('artistic/art_manage_post/' . $row['user_id']); ?>">
+                         line-height: 24px; font-weight: 600; color: #000033; margin-bottom: 4px;display: inline-block; "  href="<?php echo base_url('artistic/art_manage_post/' . $row['user_id']); ?>">
                         <?php echo ucwords($firstname) . ' ' . ucwords($lastname); ?><span style="font-weight: 400;"> <?php echo date('d-M-Y', strtotime($row['created_date'])); ?></span>
                         </a>
                           <?php }?>                          
