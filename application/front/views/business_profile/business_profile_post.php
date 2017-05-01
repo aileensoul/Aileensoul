f6e2d792355d7fe8bc62c7f31019785b8af2f8a2
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-05-01 14:30:59 +0530

    business_profile_post date

--- application/front/views/business_profile/business_profile_post.php
+++ application/front/views/business_profile/business_profile_post.php
@@ -786,14 +786,14 @@ $slugnameposted =  $this->db->get_where('business_profile',array('user_id' => $r
                   <li>
                   <div class="else_post_d">
        <div class="post-design-product">
-       <div><a style=" width:16%;font-size: 18px;  display: inline-block; line-height: 24px; font-weight: 600;  color: #000033;  margin-bottom: 4px; text-overflow: ellipsis; overflow: hidden; white-space: nowrap; " href="<?php echo base_url('business_profile/business_profile_manage_post/'.$slugnameposted); ?>"><?php echo ucwords($companynameposted); ?></a>
+       <div><a style=" max-width: 26%; width: auto;  font-size: 18px;  display: inline-block; line-height: 24px; font-weight: 600;  color: #000033;  margin-bottom: 4px; text-overflow: ellipsis; overflow: hidden; white-space: nowrap; " href="<?php echo base_url('business_profile/business_profile_manage_post/'.$slugnameposted); ?>"><?php echo ucwords($companynameposted); ?></a>
        </div> <span style="font-weight: 600;"> Posted With </span> <a style=" font-size: 18px;
                          line-height: 24px; font-weight: 600; color: #000033; margin-bottom: 4px; " href="<?php echo base_url('business_profile/business_profile_manage_post/'.$slugname); ?>"><?php echo ucwords($companyname); ?></a> <span  style="font-weight: 400; cursor: default;"><?php echo date('d-M-Y',strtotime($row['created_date'])); ?> </span> </div></div>
                          </li>
                          <?php }else{?>
                 <li>
                   <div class="post-design-product">
-                    <a style=" width: 26%;font-size: 18px;  display: inline-block; font-weight: 600;  color: #000033;   text-overflow: ellipsis; overflow: hidden; white-space: nowrap; "   href="<?php echo base_url('business_profile/business_profile_manage_post/' . $slugname); ?>" title="<?php echo ucwords($companyname); ?>";>
+                    <a style=" max-width: 26%; width: auto; font-size: 18px;  display: inline-block; font-weight: 600;  color: #000033;   text-overflow: ellipsis; overflow: hidden; white-space: nowrap; "   href="<?php echo base_url('business_profile/business_profile_manage_post/' . $slugname); ?>" title="<?php echo ucwords($companyname); ?>";>
                   <?php echo ucwords($companyname); ?>  </a>
                      <div class="datespan">  <span style="font-weight: 400; cursor: default;"> 
                         <?php echo date('d-M-Y', strtotime($row['created_date'])); ?>
