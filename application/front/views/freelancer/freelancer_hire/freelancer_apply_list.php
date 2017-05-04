139b77146b2835355a4b4a06a337cc0a70fb6431
vishang2aileensoul <vishang2aileensoul@gmail.com>
2017-05-04 15:59:23 +0530

    set invited btn design

--- application/front/views/freelancer/freelancer_hire/freelancer_apply_list.php
+++ application/front/views/freelancer/freelancer_hire/freelancer_apply_list.php
@@ -220,7 +220,7 @@
  <?php  $contition_array = array('invite_user_id' => $row['user_id'], 'post_id' => $postid, 'profile' => 'freelancer');
         $userdata = $this->common->select_data_by_condition('user_invite', $contition_array, $data = '*', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');
         if($userdata){ ?>
-          <div class="button invite_border" id="<?php echo 'invited' . $row['user_id']; ?>" > Invited</div>       
+          <div class="button invited" id="<?php echo 'invited' . $row['user_id']; ?>" > Invited</div>       
          <?php }else{ ?>
               <div class="button invite_border" id="<?php echo 'invited' . $row['user_id']; ?>" onclick="inviteuser(<?php echo $row['user_id']; ?>)"> Invite</div>
           <?php  } ?>
