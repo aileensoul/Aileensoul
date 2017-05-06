bc3b28c632d1a2333a47d7c1e9417ffe85c1aa9e
rashmi-aileensoul <rashmi.aileensoul@gmail.com>
2017-05-06 19:48:13 +0530

    varible change

--- application/front/controllers/Job.php
+++ application/front/controllers/Job.php
@@ -1412,7 +1412,7 @@ class Job extends MY_Controller {
 
         $contition_array = array('user_id' => $userid, 'is_delete' => 0, 'status' => 1);
 
-        $userdata = $this->common->select_data_by_condition('job_reg', $contition_array, $data = '*', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');
+        $userdata = $this->data['jobskill']=$this->common->select_data_by_condition('job_reg', $contition_array, $data = '*', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');
 
         if ($userdata) {
             $step = $userdata[0]['job_step'];
