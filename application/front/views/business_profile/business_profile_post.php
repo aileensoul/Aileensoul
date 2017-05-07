70394d5974bf2738169a21418e385f7d338afe5e
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-05-07 12:24:28 +0530

    post uplod

--- application/front/views/business_profile/business_profile_post.php
+++ application/front/views/business_profile/business_profile_post.php
@@ -701,9 +701,9 @@
                                     </div>
                                     <div id="myBtn1"  class="editor-content col-md-10 popup-text" >
                                       <!-- <textarea name="product_title" placeholder="Post Your Product...."></textarea>  -->
-                                        <textarea id= "test-upload-product" placeholder="Post Your Product...."  onKeyPress=check_length(this.form); onKeyDown=check_length(this.form); name=my_text rows=4 cols=30 class="post_product_name" style="height:  10%;"></textarea>
-                                        <div style="display: none;">                        
-                                            <input size=1 value=50 name=text_num style="width: 52px;" readonly> 
+                                        <textarea id= "test-upload-product" placeholder="Post Your Product...."  onKeyPress=check_length(this.form); onKeyDown=check_length(this.form); name=my_text rows=4 cols=30 class="post_product_name" style=" position: relative;"></textarea>
+                                      <div style="position: absolute; top: 21px; right: 19px; border: none;">                       
+                                            <input size=1 value=50 name=text_num style=" border: none;   width: 30px; padding: 5px; height: 30px;" readonly> 
                                         </div>
                                     </div>
                                     <!--   <span class="fr">
