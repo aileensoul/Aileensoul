f27b3ecc39016385603408948fe98afd7969707d
rashmi-aileensoul <rashmi.aileensoul@gmail.com>
2017-05-07 17:23:12 +0530

    add script fetch data add js and css

--- application/front/views/job/job_project.php
+++ application/front/views/job/job_project.php
@@ -7,6 +7,7 @@
 <?php echo $header; ?>
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/1.10.3.jquery-ui.css'); ?>">
 
 <?php echo $job_header2; ?>
 <!-- END HEADER -->
