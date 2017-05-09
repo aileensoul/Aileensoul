0a18b0b3046704b8feaad0b5215d71931dd432e2
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-05-09 16:33:14 +0530

    login

--- application/front/controllers/Contact_us.php
+++ application/front/controllers/Contact_us.php
@@ -32,34 +32,37 @@ class Contact_us extends CI_Controller {
     public function contact_us_insert() 
      { 
    
+          $name = $_POST['name'];
+          $email = $_POST['email'];
+          $subject = $_POST['subject'];
+          $message = $_POST['message'];
+
         $this->form_validation->set_rules('contact_name', 'contact name', 'required');
         $this->form_validation->set_rules('contact_email', 'contact email', 'required|valid_email');
         $this->form_validation->set_rules('contact_subject','contact subject', 'required');
         $this->form_validation->set_rules('contact_message','contact message', 'required');
         
 
-                      $data = array(
+            $data = array(
 
-                        'contact_name' => $this->input->post('contact_name'),
-                         'contact_email' => $this->input->post('contact_email'),
-                         'contact_subject' => $this->input->post('contact_subject'),
-                         'contact_message' => $this->input->post('contact_message'),
-                        
-               
+                'contact_name' =>  $name,
+                'contact_email' => $email,
+                'contact_subject' => $subject,
+                'contact_message' => $message,
                 ); 
                 //echo"<pre>";print_r($data);die();
               $insert_id =   $this->common->insert_data_getid($data,'contact_us'); 
-                if($insert_id)
-                { 
-                       $this->session->set_flashdata('success', 'successfully');
-                      redirect('main');
+               //  if($insert_id)
+               //  { 
+               //         $this->session->set_flashdata('success', 'successfully');
+               //        redirect('main');
                   
-                }
-               else
-                {
-                        $this->session->flashdata('error','Sorry!! Your data not inserted');
-                       redirect('contact_us', 'refresh');
-                   }
+               //  }
+               // else
+               //  {
+               //          $this->session->flashdata('error','Sorry!! Your data not inserted');
+               //         redirect('contact_us', 'refresh');
+               //     }
      
 }
     
