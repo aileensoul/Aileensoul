d8e3ed8bac848c570da47319c63af46ae3ab4b03
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-05-09 12:53:07 +0530

    id add

--- application/front/views/freelancer/freelancer_post/freelancer_post_professional_information.php
+++ application/front/views/freelancer/freelancer_post/freelancer_post_professional_information.php
@@ -12,7 +12,7 @@
 
 <body>
     <section>
-        <div class="user-midd-section">
+        <div class="user-midd-section" id="paddingtop_fixed">
             <div class="container">
                 <div class="row">
                     <div class="col-md-3 col-sm-3">
