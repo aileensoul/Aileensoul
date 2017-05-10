c2456ae4042140c81ea493c2586c8222324a4480
ankit-aileensoul <ankit.aileensoul@gmail.com>
2017-05-10 15:52:17 -0700

    random function add in my controller

--- application/front/core/MY_Controller.php
+++ application/front/core/MY_Controller.php
@@ -7,47 +7,63 @@ class MY_Controller extends CI_Controller {
 
     function __construct() {
         parent::__construct();
-     // echo $this->session->userdata('aileenuser');  echo "hello"; die();
+        // echo $this->session->userdata('aileenuser');  echo "hello"; die();
         if (!$this->session->userdata('aileenuser')) {
             redirect('login', 'refresh');
         } else {
             $this->data['userid'] = $this->session->userdata('aileenuser');
         }
 
-        
-        $user_id=$this->data['userid'];
-        $condition_array=array('status' => '1');
-        $this->data['loged_in_user']=$this->common->select_data_by_id('user','user_id',$user_id,'user_name,user_image',$condition_array);
-        
-        
+
+        $user_id = $this->data['userid'];
+        $condition_array = array('status' => '1');
+        $this->data['loged_in_user'] = $this->common->select_data_by_id('user', 'user_id', $user_id, 'user_name,user_image', $condition_array);
     }
 
-   public  function time_elapsed_string($datetime, $full = false) { 
-    $now = new DateTime;
-    $ago = new DateTime($datetime);
-    $diff = $now->diff($ago);
+    public function time_elapsed_string($datetime, $full = false) {
+        $now = new DateTime;
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
 
-    $diff->w = floor($diff->d / 7);
-    $diff->d -= $diff->w * 7;
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
 
-    $string = array(
-        'y' => 'year',
-        'm' => 'month',
-        'w' => 'week',
-        'd' => 'day',
-        'h' => 'hour',
-        'i' => 'minute',
-        's' => 'second',
-    );
-    foreach ($string as $k => &$v) {
-        if ($diff->$k) {
-            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
-        } else {
-            unset($string[$k]);
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
         }
+
+        if (!$full)
+            $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
     }
 
-    if (!$full) $string = array_slice($string, 0, 1);
-    return $string ? implode(', ', $string) . ' ago' : 'just now'; 
-}
+    public function random_string($length = 10, $allowed_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890') {
+        $allowed_chars_len = strlen($allowed_chars);
+
+        if ($allowed_chars_len == 1) {
+            return str_pad('', $length, $allowed_chars);
+        } else {
+            $result = '';
+
+            while (strlen($result) < $length) {
+                $result .= substr($allowed_chars, rand(0, $allowed_chars_len), 1);
+            } // while
+
+            return $result;
+        } 
+    }
+
 }
