020a379c4f071e32a449a44bde8d4cbd7eb62038
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-05-10 10:40:29 +0530

    style add

--- application/front/views/job/job_header2.php
+++ application/front/views/job/job_header2.php
@@ -1,138 +1,20 @@
-<!--post save success pop up style strat -->
-<style>
-body {
-  font-family: Arial, sans-serif;
-  background-size: cover;
-  height: 100vh;
-}
 
-.box {
-  width: 40%;
-  margin: 0 auto;
-  background: rgba(255,255,255,0.2);
-  padding: 35px;
-  border: 2px solid #fff;
-  border-radius: 20px/50px;
-  background-clip: padding-box;
-  text-align: center;
-}
-
-
-
-.overlay {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: rgba(0, 0, 0, 0.3);
-  transition: opacity 500ms;
-  visibility: hidden;
-  opacity: 0;
-  z-index: 10;
-}
-.overlay:target {
-  visibility: visible;
-  opacity: 1;
-}
-
-.popup {
-    margin: 70px auto;
-    padding: 20px;
-    background: #fff;
-    border-radius: 5px;
-    width: 30%;
-    height: 200px;
-    position: relative;
-    transition: all 5s ease-in-out;
-}
-
-.okk{
-  text-align: center;
-}
-
-.popup .okbtnpop {
-  position: absolute;
-    transition: all 200ms;
-    font-size: 18px;
-    font-weight: bold;
-    text-decoration: none;
-    color: #fff;
-    padding: 8px 18px;
-    background-color: darkcyan;
-    left: 25px;
-    margin-top: 15px;
-    width: 100px; 
-    border-radius: 8px;
-}
-
-.popup .cnclbtn {
-  position: absolute;
-    transition: all 200ms;
-    font-size: 18px;
-    font-weight: bold;
-    text-decoration: none;
-    color: #fff;
-    padding: 8px 18px;
-    background-color: darkcyan;
-    right: 25px;
-    margin-top: 15px;
-    width: 100px;
-    border-radius: 8px;
-}
-
-.popup .pop_content {
- text-align: center;
- margin-top: 40px;
-  
-}
-
-@media screen and (max-width: 700px){
-  .box{
-    width: 70%;
-  }
-  .popup{
-    width: 70%;
-  }
-}
-</style>
 
 <!--post save success pop up style end -->
 
 
 <style type="text/css">
-  .dropdown_hover {
-    position: relative;
-    display: inline-block;
-    color: white;
-    padding: 8px 16px 8px;
-    cursor: pointer;
-}
-
-
-.dropdown-content_hover {
-    display: none;
+  
+ .dropdown-content_hover{   display: none;
     position: absolute;
     color: #3b5283;
     background-color: #fff;
-    min-width: 160px;
+    min-width: 139px;
     box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    margin-top: 10px;
+    margin-top: 3px;
     z-index: 1;
-    border-radius:4px;
-}
-
-.dropdown_hover .dropdown-content_hover a {color: #000;}
-
-/* vishang 9-5-17 header2 on */
-
-
-
-   /* Firefox 18- */
-
-
-/* vishang 9-5-17 header2 off */
-
+    left: -25px;
+    border-radius: 4px;}
 </style>
    
 <header>
