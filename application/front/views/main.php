bd447997df6d92c7bd6303bb566c8cf4279914c2
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-05-12 11:27:15 +0530

    auto compelte off at a make profile time when save on browser

--- application/front/views/main.php
+++ application/front/views/main.php
@@ -187,7 +187,7 @@
           <div class="top-row">
             <div class="field-wrap full-width">
              <label class="fname_reg">Email Address <span class="req">*</span></label>
-             <input type="text" name="email" id="email"  autocomplete='off'>
+             <input type="text" name="email" id="email" placeholder="" value="<?php if($email){ echo $email; }?>">
               <?php echo form_error('email'); ?>
             </div>
         
@@ -203,7 +203,7 @@
          <div class="top-row">
             <div class="field-wrap full-width">
         <label class="fname_reg">Password<span class="req">*</span></label>
-    <input type="password" name="password" id="password" class="showpassword"  style="position: relative;" autocomplete='off'/>
+    <input type="password" name="password" id="password" class="showpassword" placeholder="Password *" style="position: relative;"  />
                       <?php echo form_error('password'); ?>
             
 
