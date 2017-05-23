b8453d9420f1634236028b9e9909ad0baec1e508
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-05-23 13:17:13 +0530

    Dhaval add new page header border

---
+++ application/front/views/freelancer/freelancer_hire/freelancer_hire_header2_border.php
@@ -0,0 +1,177 @@
+<!--post save success pop up style strat -->
+
+<style>
+  
+    .okk{
+        text-align: center;
+    }
+
+    .pop_content .okbtn{
+        position: absolute;
+        transition: all 200ms;
+        font-size: 16px;
+        text-decoration: none;
+        color: #fff;
+        padding: 8px 18px;
+        background-color: #0A2C5D;
+        left: 170px;
+        margin-top: 8px;
+        width: 100px; 
+        border-radius: 8px;
+    }
+
+  
+    .pop_content .cnclbtn {
+        position: absolute;
+        transition: all 200ms;
+        font-size: 16px;
+        text-decoration: none;
+        color: #fff;
+        padding: 8px 18px;
+        background-color: #0A2C5D;
+        right: 170px;
+        margin-top: 8px;
+        width: 100px;
+        border-radius: 8px;
+    }
+
+    .popup .pop_content {
+        text-align: center;
+        margin-top: 40px;
+
+    }
+    .model_ok_cancel{
+        width:200px !important;
+    }
+
+
+
+</style>
+
+
+<style type="text/css">
+  .dropdown-content_hover::before {
+    /* top: -1px; */
+    content: '';
+    display: block;
+    position: absolute;
+    width: 0;
+    height: 0;
+    color: transparent;
+    border: 9px solid black;
+    border-color: transparent transparent #fff;
+    margin-top: -18px;
+    /* margin-left: 104px; */
+    right: 11px;
+}
+</style>
+<header>
+    <div class="bg-search">
+        <div class="header2 headerborder">
+            <div class="container">
+                <div class="row">
+                     <?php echo $freelancer_hire_search; ?>
+                  <div class="col-md-5 col-sm-5">
+                       <div class="">
+                            <ul class="">
+                               
+                                  <li <?php if(($this->uri->segment(1) == 'freelancer') && ($this->uri->segment(2) == 'freelancer_hire_post')){?> class="active" <?php } ?>><a href="<?php echo base_url('freelancer/recommen_candidate'); ?>">Home</a>
+                                    </li>
+    
+                                <!-- Friend Request Start-->
+  <li>
+                                <!-- Friend Request End-->
+
+<div class="dropdown_hover">
+  <span id="art_profile">Employer Profile <i class="fa fa-angle-down" aria-hidden="true"></i></span>
+  <div class="dropdown-content_hover" id="dropdown-content_hover">
+    <a href="<?php echo base_url('freelancer/freelancer_hire_profile'); ?>"><i class="fa fa-user" aria-hidden="true"></i> View Profile</a>
+    <a href="<?php echo base_url('freelancer_hire/freelancer_hire_basic_info'); ?>"><i class="fa fa-pencil" aria-hidden="true"></i> Edit Profile</a>
+     <?php
+      $userid = $this->session->userdata('aileenuser');
+      ?>
+
+    <a onClick="deactivate(<?php echo $userid; ?>)"><i class="fa fa-minus-circle" aria-hidden="true"></i> Deactive Profile</a>
+  </div>
+</div>
+</li>
+                                <!-- Friend Request End-->
+
+                                <!-- END USER LOGIN DROPDOWN -->
+                            </ul>
+                        </div> 
+                    </div>
+                  
+                
+                </div>
+            </div>
+        </div>
+       </div> 
+    </header>
+
+
+<!-- Bid-modal  -->
+                    <div class="modal fade message-box biderror" id="bidmodal" role="dialog">
+                        <div class="modal-dialog modal-lm">
+                            <div class="modal-content">
+                                <button type="button" class="modal-close" data-dismiss="modal">&times;</button>       
+                                <div class="modal-body">
+                                    <!--<img class="icon" src="images/dollar-icon.png" alt="" />-->
+                                    <span class="mes"></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Model Popup Close -->
+
+
+    <script type="text/javascript">
+  
+
+$(document).ready(function(){
+    $('.dropdown_hover').click(function(event){
+        event.stopPropagation();
+         $(".dropdown-content_hover").slideToggle("fast");
+    });
+    $(".dropdown-content_hover").on("dropdown_hover", function (event) {
+        event.stopPropagation();
+    });
+});
+
+$(document).on("dropdown_hover", function () {
+    $(".dropdown-content_hover").hide();
+});
+
+$(document).ready(function() {
+     $("body").click(function(event) {
+        $(".dropdown-content_hover").hide();
+        event.stopPropagation();
+    });
+ 
+});
+</script>
+
+
+<script type="text/javascript">
+
+  function deactivate(clicked_id) { 
+      $('.biderror .mes').html("<div class='pop_content'> Are you sure you want to deactive your Freelancer_Hire_profile?.<div class='model_ok_cancel'><a class='okbtn' id=" + clicked_id + " onClick='deactivate_profile(" + clicked_id + ")' href='javascript:void(0);' data-dismiss='modal'>Yes</a><a class='cnclbtn' href='javascript:void(0);' data-dismiss='modal'>No</a></div></div>");
+          $('#bidmodal').modal('show');
+ }
+
+ function deactivate_profile(clicked_id){
+
+                  $.ajax({
+                      type: 'POST',
+                      url: '<?php echo base_url() . "freelancer/deactivate_hire" ?>',
+                      data: 'id=' + clicked_id,
+                        success: function (data) {
+                          window.location= "<?php echo base_url() ?>dashboard";
+                                    
+                                }
+                            });
+
+
+
+ }
+ </script>
