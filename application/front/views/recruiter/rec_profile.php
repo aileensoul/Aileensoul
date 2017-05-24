d303ecebf8a85ec2b5366ae3ba11aee92ec49d6b
rashmi-aileensoul <rashmi.aileensoul@gmail.com>
2017-05-24 12:34:21 +0530

    desiganation change

--- application/front/views/recruiter/rec_profile.php
+++ application/front/views/recruiter/rec_profile.php
@@ -994,6 +994,9 @@ function myFunction() {
                             function editableTextBlurred() {
                                 var html = $(this).val();
                                 var viewableText = $("<a>");
+                                 if(html == ''){
+            html = "Designation";
+        }
                                 viewableText.html(html);
                                 $(this).replaceWith(viewableText);
                                 // setup the click event for this new div
