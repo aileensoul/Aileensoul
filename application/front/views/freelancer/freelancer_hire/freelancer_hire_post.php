0af9d32a060638656f7daa782bc4803f26056872
rashmi-aileensoul <rashmi.aileensoul@gmail.com>
2017-05-24 12:45:14 +0530

    designation

--- application/front/views/freelancer/freelancer_hire/freelancer_hire_post.php
+++ application/front/views/freelancer/freelancer_hire/freelancer_hire_post.php
@@ -1132,6 +1132,9 @@ $( "#tags" ).autocomplete({
                             function editableTextBlurred() {
                                 var html = $(this).val();
                                 var viewableText = $("<a>");
+                                  if(html == ''){
+            html = "Designation";
+        }
                                 viewableText.html(html);
                                 $(this).replaceWith(viewableText);
                                 // setup the click event for this new div
