d254a3592bfec69ca6f88a01545e1ee9eb6e328f
rashmi-aileensoul <rashmi.aileensoul@gmail.com>
2017-05-24 20:00:58 +0530

    change css js for search

--- application/front/views/artistic/art_post.php
+++ application/front/views/artistic/art_post.php
@@ -1,6 +1,10 @@
 <!-- start head -->
 
 <?php echo $head; ?>
+<link rel="stylesheet" href="<?php echo base_url() ?>css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/1.10.3.jquery-ui.css'); ?>">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+
 
 <!--post save success pop up style strat -->
 <style>
@@ -1706,12 +1710,12 @@ if (count($finalsorting) > 0) {
                     </script>
                     <!-- script for skill textbox automatic start (option 2)-->
 
-<!--                    <script src="<?php echo base_url('js/jquery.wallform.js'); ?>"></script>
+                    <script src="<?php echo base_url('js/jquery.wallform.js'); ?>"></script>
                     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
                     <script src="<?php echo base_url('js/jquery-ui.min.js'); ?>"></script>
                     <script src="<?php echo base_url('js/demo/jquery-1.9.1.js'); ?>"></script>
                     <script src="<?php echo base_url('js/demo/jquery-ui-1.9.1.js'); ?>"></script>
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>-->
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
 
 
                     <!-- script for skill textbox automatic end (option 2)-->
