f7ee77895b4baf7ab03e9664eedc8c6257d254f8
rashmi-aileensoul <rashmi.aileensoul@gmail.com>
2017-05-27 16:45:06 +0530

    div remove in cover pic fetch]

--- application/front/views/freelancer/freelancer_hire/freelancer_hire_post.php
+++ application/front/views/freelancer/freelancer_hire/freelancer_hire_post.php
@@ -91,13 +91,13 @@ echo $freelancer_hire_header2;} ?>
                     $image_ori = $image[0]['profile_background'];
                     if ($image_ori) {
                         ?>
-                        <div class="bg-images">
-                            <img src="<?php echo base_url($this->config->item('free_hire_bg_main_upload_path').$image[0]['profile_background']);?>" name="image_src" id="image_src" / ></div>
+                        
+                            <img src="<?php echo base_url($this->config->item('free_hire_bg_main_upload_path').$image[0]['profile_background']);?>" name="image_src" id="image_src" / >
                         <?php
                     } else {
                         ?>
-                        <div class="bg-images">
-                            <img src="<?php echo WHITEIMAGE; ?>" name="image_src" id="image_src" / ></div>
+                        
+                            <img src="<?php echo WHITEIMAGE; ?>" name="image_src" id="image_src" / >
                     <?php }
                     ?>
 
