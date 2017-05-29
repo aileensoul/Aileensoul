478e1221e8efa47393ad7f82e84d93a2586d654d
rashmi-aileensoul <rashmi.aileensoul@gmail.com>
2017-05-29 12:20:44 +0530

    insert image in edit businee profile

--- application/front/controllers/Business_profile.php
+++ application/front/controllers/Business_profile.php
@@ -736,19 +736,50 @@ class Business_profile extends MY_Controller {
 
                 $this->upload->initialize($config);
                 $this->upload->do_upload();
+
                 if ($this->upload->do_upload('image1')) {
-                    $fileData = $this->upload->data();
-                    $uploadData[$i]['file_name'] = $fileData['file_name'];
-                } else {
-                    $uploadData[$i]['file_name'] = '';
+
+                    // $fileData = $this->upload->data();
+                    // $uploadData[$i]['file_name'] = $fileData['file_name'];
+                     $response['result'][] = $this->upload->data();
+                        $business_profile_post_thumb[$i]['image_library'] = 'gd2';
+                        $business_profile_post_thumb[$i]['source_image'] = $this->config->item('bus_profile_main_upload_path') . $response['result'][$i]['file_name'];
+                        $business_profile_post_thumb[$i]['new_image'] = $this->config->item('bus_profile_thumb_upload_path') . $response['result'][$i]['file_name'];
+                        $business_profile_post_thumb[$i]['create_thumb'] = TRUE;
+                        $business_profile_post_thumb[$i]['maintain_ratio'] = TRUE;
+                        $business_profile_post_thumb[$i]['thumb_marker'] = '';
+                        $business_profile_post_thumb[$i]['width'] = $this->config->item('bus_profile_thumb_width');
+                        //$product_thumb[$i]['height'] = $this->config->item('product_thumb_height');
+                        $business_profile_post_thumb[$i]['height'] = 2;
+                        $business_profile_post_thumb[$i]['master_dim'] = 'width';
+                        $business_profile_post_thumb[$i]['quality'] = "100%";
+                        $business_profile_post_thumb[$i]['x_axis'] = '0';
+                        $business_profile_post_thumb[$i]['y_axis'] = '0';
+                        $instanse = "image_$i";
+                        //Loading Image Library
+                        $this->load->library('image_lib', $business_profile_post_thumb[$i], $instanse);
+                        $dataimage = $response['result'][$i]['file_name'];
+                        //Creating Thumbnail
+                        $this->$instanse->resize();
+                        $response['error'][] = $thumberror = $this->$instanse->display_errors();
+
+                        $return['data'][] = $imgdata;
+                        $return['status'] = "success";
+                        $return['msg'] = sprintf($this->lang->line('success_item_added'), "Image", "uploaded");
+
+
+                }
+
+                 else {
+                    $dataimage = '';
                 }
 
 
 
-                if ($uploadData[$i]['file_name']) {
+                if ($dataimage) {
 //echo $uploadData[$i]['file_name'];
                     $data = array(
-                        'image_name' => $uploadData[$i]['file_name'],
+                        'image_name' => $dataimage,
                         'user_id' => $userid,
                         'created_date' => date('Y-m-d H:i:s'),
                         'is_delete' => 0
@@ -758,7 +789,7 @@ class Business_profile extends MY_Controller {
                 }
 
 
-                if ($uploadData[$i]['file_name']) {
+                if ($dataimage) {
                     $data = array(
                         'modified_date' => date('Y-m-d', time()),
                         'business_step' => 4
