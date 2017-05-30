d9763b674040dce37c09f43dc3f0d30c2b56bfce
pallavi-aileensoul <pallavi.alieensoul@gmail.com>
2017-05-30 13:13:41 +0530

    change for designation criteria

--- application/front/views/recruiter/saved_candidate.php
+++ application/front/views/recruiter/saved_candidate.php
@@ -815,9 +815,9 @@ $( "#tags" ).autocomplete({
                             function editableTextBlurred() {
                                 var html = $(this).val();
                                 var viewableText = $("<a>");
-                                if(html == ''){
-            html = "Current Work";
-        }
+                                if (html.match(/^\s*$/) || html == '') { 
+                                html = "Designation";
+                                }
                                 viewableText.html(html);
                                 $(this).replaceWith(viewableText);
                                 // setup the click event for this new div
