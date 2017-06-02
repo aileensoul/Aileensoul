be333132f173dbb408fb814f44b7a377f81b5e3c
rashmi-aileensoul <rashmi.aileensoul@gmail.com>
2017-06-02 15:27:47 +0530

    change location search code

--- application/front/controllers/Job.php
+++ application/front/controllers/Job.php
@@ -2026,19 +2026,30 @@ class Job extends MY_Controller {
     public function location() {
         $json = [];
 
-        //$this->load->database('aileensoul');
+        $this->load->database('aileensoul');
+
+        // if (!empty($this->input->get("q"))) {
+        //     $this->db->like('city_name', $this->input->get("q"));
+        //     $query = $this->db->select('city_id as id,city_name as text')
+        //             ->order_by("city_name", "asc")
+        //             ->limit(10)
+        //             ->get("cities");
+        //     $json = $query->result();
+        // }
+
+
+        // echo json_encode($json);
 
         if (!empty($this->input->get("q"))) {
-            $this->db->like('city_name', $this->input->get("q"));
-            $query = $this->db->select('city_id as id,city_name as text')
-                    ->order_by("city_name", "asc")
-                    ->limit(10)
-                    ->get("cities");
-            $json = $query->result();
-        }
+     $search_condition = "(city_name LIKE '" . trim($this->input->get("q")) . "%')";
 
-
-        echo json_encode($json);
+     $tolist = $this->common->select_data_by_search('cities', $search_condition,$contition_array = array(), $data = 'city_id as id,city_name as text', $sortby = 'city_name', $orderby = 'desc', $limit = '', $offset = '', $join_str5 = '', $groupby = '');
+   
+//echo '<pre>'; print_r($tolist); die();
+     }
+      //  echo json_encode($tolist);
+        echo json_encode($tolist);
+    
     }
 
 //location automatic retrieve controller End
