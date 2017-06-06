be77b0f3285b5789430d78b70583112cd3ccf4b9
dhaval-aileensoul <dhavalshah.aileensoul43@gmail.com>
2017-06-06 15:53:24 +0530

    add the class in freelancer apply lit

--- application/front/views/freelancer/freelancer_hire/freelancer_apply_list.php
+++ application/front/views/freelancer/freelancer_hire/freelancer_apply_list.php
@@ -228,7 +228,7 @@
               <!--<a href="<?php echo base_url('chat/abc/' . $row['user_id']); ?>">Saved</a>-->
            
           <?php if($userid != $row['user_id']){ ?>
-       <a href="<?php echo base_url('chat/abc/' . $row['user_id']); ?>">Message</a>
+       <a class="msg_btn" href="<?php echo base_url('chat/abc/' . $row['user_id']); ?>">Message</a>
 
  <?php  $contition_array = array('invite_user_id' => $row['user_id'], 'post_id' => $postid, 'profile' => 'freelancer');
         $userdata = $this->common->select_data_by_condition('user_invite', $contition_array, $data = '*', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');
