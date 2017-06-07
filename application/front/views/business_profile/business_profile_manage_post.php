c7f596ef2030f12f6040298da1674cf018c48b09
ankit-aileensoul <ankit.aileensoul@gmail.com>
2017-06-07 16:55:53 -0400

    Less More Bugs

--- application/front/views/business_profile/business_profile_manage_post.php
+++ application/front/views/business_profile/business_profile_manage_post.php
@@ -3540,10 +3540,11 @@
             <script>
                 $(function () {
 //                    var showTotalChar = 200, showChar = "More", hideChar = "Less";
-                    var showTotalChar = 250, showChar = "More", hideChar = "";
+                    var showTotalChar = 180, showChar = "More", hideChar = "";
                     $('.show').each(function () {
                         //var content = $(this).text();
                         var content = $(this).html();
+                        content = content.replace(/ /g,'');
                         if (content.length > showTotalChar) {
                             var con = content.substr(0, showTotalChar);
                             var hcon = content.substr(showTotalChar, content.length - showTotalChar);
