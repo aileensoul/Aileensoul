190882388dea67aff5b33a9c41ee3d6138fc449c
rashmi-aileensoul <rashmi.aileensoul@gmail.com>
2017-06-08 11:04:59 +0530

    search location change

--- application/front/views/artistic/artistic_userlist.php
+++ application/front/views/artistic/artistic_userlist.php
@@ -11,7 +11,7 @@
 <!--<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">-->
 <link rel="stylesheet" href="<?php echo base_url() ?>css/bootstrap.min.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/1.10.3.jquery-ui.css'); ?>">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+
 <style type="text/css" media="screen">
     #row2 { overflow: hidden;  width: 100%; }
     #row2 img { height: 350px;width: 100%; }
@@ -405,7 +405,7 @@
 <script src="<?php echo base_url('js/demo/jquery-1.9.1.js'); ?>"></script>
 <script src="<?php echo base_url('js/demo/jquery-ui-1.9.1.js'); ?>"></script>
 <script src="<?php echo base_url('assets/js/croppie.js'); ?>"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+
 
 
 <!-- designation script start -->
@@ -482,6 +482,39 @@
 
 </script>
 
+<script>
+
+                                                        var data1 = <?php echo json_encode($de); ?>;
+// alert(data);
+
+
+                                                        $(function () {
+                                                            // alert('hi');
+                                                            $("#searchplace").autocomplete({
+                                                                source: function (request, response) {
+                                                                    var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(request.term), "i");
+                                                                    response($.grep(data1, function (item) {
+                                                                        return matcher.test(item.label);
+                                                                    }));
+                                                                },
+                                                                minLength: 1,
+                                                                select: function (event, ui) {
+                                                                    event.preventDefault();
+                                                                    $("#searchplace").val(ui.item.label);
+                                                                    $("#selected-tag").val(ui.item.label);
+                                                                    // window.location.href = ui.item.value;
+                                                                }
+                                                                ,
+                                                                focus: function (event, ui) {
+                                                                    event.preventDefault();
+                                                                    $("#searchplace").val(ui.item.label);
+                                                                }
+                                                            });
+                                                        });
+
+</script>
+
+
 <script type="text/javascript">
     function checkvalue() {
         //alert("hi");
@@ -496,7 +529,7 @@
     }
 </script>
 
-<script>
+<!-- <script>
 //select2 autocomplete start for skill
     $('#searchskills').select2({
 
@@ -550,7 +583,7 @@
 //select2 autocomplete End for Location
 
 </script>
-<!-- popup form edit start -->
+ --><!-- popup form edit start -->
 
 <script>
 // Get the modal
