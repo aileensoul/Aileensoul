f6dea70f70d74b7ee0c6da881f3adf763b81f57f
ankit-aileensoul <ankit.aileensoul@gmail.com>
2017-06-09 18:41:46 -0400

    eod 9-6-17

--- application/front/views/job/job_search.php
+++ application/front/views/job/job_search.php
@@ -1,11 +1,11 @@
  <?php
                          echo form_open('search/job_search'); ?>
-                            <fieldset class="col-md-3">
+                            <fieldset class="col-md-3 col-sm-5 col-xs-5">
                             
                                  <input type="text" id="tags" name="skills" placeholder="Find Your Job">
                                 </select>
                             </fieldset>
-                            <fieldset class="col-md-3">
+                            <fieldset class="col-md-3 col-sm-5 col-xs-5">
                             
                               <input type="text" id="searchplace" name="searchplace" placeholder="Find Your location">
                               <!-- <select class="" name="searchplace[]" id="searchplace" multiple="multiple"  placeholder="Find Your location"> -->
@@ -21,7 +21,7 @@
                                 <button onclick="return checkvalue();"> Search</button>
                             </fieldset>
                              -->
-                            <fieldset class="col-md-2">
+                            <fieldset class="col-md-2 col-sm-2 col-xs-2">
                                   <label for="search_btn" id="search_f"><i class="fa fa-search" aria-hidden="true"></i></label>
                                 <button onclick="return checkvalue();"  id="search_btn" style="display: none;"> Search</button>
                             </fieldset>
