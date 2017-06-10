940810161355fff1e08ce222f9e0cdeb10858aab
aarati-aileensoul <aarati.aileensoul@gmail.com>
2017-06-10 11:41:13 +0530

    edit_post leave page on click on all button

--- application/front/views/recruiter/rec_search.php
+++ application/front/views/recruiter/rec_search.php
@@ -22,7 +22,7 @@
                                 <?php if(($this->uri->segment(1) == 'recruiter' && $this->uri->segment(2) == 'add_post') || ($this->uri->segment(1) == 'recruiter' && $this->uri->segment(2) == 'edit_post')){?>
                                 
                                  <label for="search_btn" id="search_f"><i class="fa fa-search" aria-hidden="true"></i></label>
-                               <input type="button" name="search_submit" value="Search" onclick="return leave_page(4)"    id="search_btn" style="display: none;">
+                               <input type="button" name="search_submit" value="Search" onclick="return leave_page(4)"   id="search_btn" style="display: none;">
                               <!--  <button type="button" class="btn btn-primary btn-block" data-title="Delete" onclick="return leave_page(4)" data-toggle="modal" data-target="#delete" >SIGN UP AS SKATER</button> -->
                                <!--   <a href="javascript:void(0);" name="search_submit" value="Search"  id="search_btn" style="color:white;"  onclick="return leave_page(4)">search</a> -->
 
