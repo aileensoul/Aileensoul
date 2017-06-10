33ad43e28feded4b529310ceada478f432dd1e64
pallavi-aileensoul <pallavi.alieensoul@gmail.com>
2017-06-10 12:00:27 +0530

    grade validation not mandotary

--- application/front/views/job/job_education.php
+++ application/front/views/job/job_education.php
@@ -631,7 +631,11 @@
 
 
                                                             <fieldset class="full-width">
+<<<<<<< HEAD
+                                          <h6>Grade :</h6>
+=======
                                           <h6>Grade :<!-- <span class="red">*</span> --></h6>
+>>>>>>> ed6997f8e56a343bdc58309da57c37b90e875c87
                                    <input type="text" name="grade[]" id="grade1" class="grade" placeholder="Enter Grade" value="<?php
                                      if ($grade1) {
                                      echo $grade1;
@@ -832,7 +836,11 @@
                                                      
                                                      
                                                            <fieldset class=""> -->
+<<<<<<< HEAD
+                                                        <h6>Grade :</h6>
+=======
                                                         <h6>Grade :<!-- <span class="red">*</span> --></h6>
+>>>>>>> ed6997f8e56a343bdc58309da57c37b90e875c87
                                                         <input type="text" name="grade[]" id="grade1" class="grade" placeholder="Enter Grade" value="<?php
                                                         if ($grade1) {
                                                             echo $grade1;
@@ -1468,7 +1476,16 @@ $.validator.addMethod("regx1", function(value, element, regexpr) {
 
 
 $.validator.addMethod("regx", function(value, element, regexpr) {          
-    return regexpr.test(value);
+    //return value == '' || value.trim().length != 0; 
+     if(!value) 
+            {
+                return true;
+            }
+            else
+            {
+                  return regexpr.test(value);
+            }
+     // return regexpr.test(value);
 }, "This is not allow");
 
                             $("#jobseeker_regform").validate({
@@ -1501,8 +1518,13 @@ $.validator.addMethod("regx", function(value, element, regexpr) {
                                     },
                                     // 'grade[]': {
 
+<<<<<<< HEAD
+                                        
+                                        regx:/^[a-zA-Z+-]/
+=======
                                     //     required: true,
                                     //     regx:/^[a-zA-Z+-]/
+>>>>>>> ed6997f8e56a343bdc58309da57c37b90e875c87
 
                                     // },
                                     'percentage[]': {
