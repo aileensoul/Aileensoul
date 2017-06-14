1e44aa8b527dd52a9aacb0970723b6d5813fad00
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-06-14 12:12:08 +0530

    esc profile pic

--- application/front/views/artistic/artistic_profile.php
+++ application/front/views/artistic/artistic_profile.php
@@ -1187,3 +1187,18 @@ $('#bidmodal-2').modal('show');
             $('#bidmodal').modal('show');
                         }
       </script>
+
+
+      <!-- all popup close close using esc start -->
+ <script type="text/javascript">
+   
+
+    $( document ).on( 'keydown', function ( e ) {
+    if ( e.keyCode === 27 ) {
+        //$( "#bidmodal" ).hide();
+        $('#bidmodal-2').modal('hide');
+    }
+});  
+
+ </script>
+ <!-- all popup close close using esc end -->
