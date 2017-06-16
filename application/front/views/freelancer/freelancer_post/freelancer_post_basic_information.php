a527ca398404c17e2994fe4f74bfe0480d195fdc
ankit-aileensoul <ankit.aileensoul@gmail.com>
2017-06-16 17:24:09 -0400

    freelance apply edit profile structur chane in mobile....

--- application/front/views/freelancer/freelancer_post/freelancer_post_basic_information.php
+++ application/front/views/freelancer/freelancer_post/freelancer_post_basic_information.php
@@ -50,29 +50,29 @@
                     <div class="col-md-3 col-sm-3">
                         <div class="left-side-bar">
 
-                            <ul>
-                                <li <?php if ($this->uri->segment(1) == 'freelancer') { ?> class="active" <?php } ?>><a href="#">Basic Information</a></li>
+                            <ul  class="left-form-each">
+                                <li <?php if ($this->uri->segment(1) == 'freelancer') { ?> class="active init" <?php } ?>><a href="#">Basic Information</a></li>
 
-                                <li class="<?php if ($freepostdata[0]['free_post_step'] < '1') {
+                                <li class="custom-none  <?php if ($freepostdata[0]['free_post_step'] < '1') {
     echo "khyati";
 } ?>"><a href="<?php echo base_url('freelancer/freelancer_post_address_information'); ?>">Address Information</a></li>
 
-                                <li class="<?php if ($freepostdata[0]['free_post_step'] < '2') {
+                                <li class="custom-none  <?php if ($freepostdata[0]['free_post_step'] < '2') {
     echo "khyati";
 } ?>"><a href="<?php echo base_url('freelancer/freelancer_post_professional_information'); ?>">Professional Information</a></li>
 
-                                <li class="<?php if ($freepostdata[0]['free_post_step'] < '3') {
+                                <li class="custom-none  <?php if ($freepostdata[0]['free_post_step'] < '3') {
     echo "khyati";
 } ?>"><a href="<?php echo base_url('freelancer/freelancer_post_rate'); ?>">Rate</a></li>
 
-                                <li class="<?php if ($freepostdata[0]['free_post_step'] < '4') {
+                                <li class="custom-none  <?php if ($freepostdata[0]['free_post_step'] < '4') {
     echo "khyati";
 } ?>"><a href="<?php echo base_url('freelancer/freelancer_post_avability'); ?>">ADD Your Avability</a></li>
 
-                                <li class="<?php if ($freepostdata[0]['free_post_step'] < '5') {
+                                <li class="custom-none  <?php if ($freepostdata[0]['free_post_step'] < '5') {
     echo "khyati";
 } ?>"><a href="<?php echo base_url('freelancer/freelancer_post_education'); ?>"> Education</a></li>		    
-                                <li class="<?php if ($freepostdata[0]['free_post_step'] < '6') {
+                                <li class="custom-none  <?php if ($freepostdata[0]['free_post_step'] < '6') {
                                 echo "khyati";
                             } ?>"><a href="<?php echo base_url('freelancer/freelancer_post_portfolio'); ?>">Portfolio</a></li>
                             </ul>
