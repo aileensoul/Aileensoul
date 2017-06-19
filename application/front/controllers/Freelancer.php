3a35a510905d3b5bf3d3c97e215e5e96f7d1e3ac
pallavi-aileensoul <pallavi.alieensoul@gmail.com>
2017-06-19 12:12:01 +0530

    city blank problem

--- application/front/controllers/Freelancer.php
+++ application/front/controllers/Freelancer.php
@@ -340,20 +340,24 @@ class Freelancer extends MY_Controller {
         $contition_array = array('status' => 1);
         $this->data['countries'] = $this->common->select_data_by_condition('countries', $contition_array, $data = '*', $sortby = 'country_name', $orderby = 'ASC', $limit = '', $offset = '', $join_str = array(), $groupby = '');
 
+        //user data fetch
+
+         $contition_array = array('user_id' => $userid, 'is_delete' => '0', 'status' => '1');
+        $userdata = $this->common->select_data_by_condition('freelancer_post_reg', $contition_array, $data = '*', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');
+
+
         //for getting state data
-        $contition_array = array('status' => 1);
+        $contition_array = array('status' => 1,'country_id' => $userdata[0]['freelancer_post_country']);
         $this->data['states'] = $this->common->select_data_by_condition('states', $contition_array, $data = '*', $sortby = 'state_name', $orderby = 'ASC', $limit = '', $offset = '', $join_str = array(), $groupby = '');
 
         //for getting city data
-        $contition_array = array('status' => 1);
-        $citiess=$this->data['cities'] = $this->common->select_data_by_condition('cities', $contition_array, $data = '*', $sortby = 'city_name', $orderby = 'ASC', $limit = '', $offset = '', $join_str = array(), $groupby = '');
+        $contition_array = array('status' => 1,'state_id'=> $userdata[0]['freelancer_post_state']);
+        $this->data['cities'] = $this->common->select_data_by_condition('cities', $contition_array, $data = '*', $sortby = 'city_name', $orderby = 'ASC', $limit = '', $offset = '', $join_str = array(), $groupby = '');
 
 
 
         //for getting job registration table data    
-        $contition_array = array('user_id' => $userid, 'is_delete' => '0', 'status' => '1');
-        $userdata = $this->common->select_data_by_condition('freelancer_post_reg', $contition_array, $data = '*', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');
-
+       
 
 
         if ($userdata) {
@@ -406,8 +410,8 @@ class Freelancer extends MY_Controller {
           }
 
 
- // $contition_array = array('status' => '1');
- //          $location_list = $this->common->select_data_by_condition('cities', $contition_array, $data = 'city_name', $sortby = '', $orderby = '', $limit = '', $offset = '', $$join_str = array(), $groupby);
+            $contition_array = array('status' => '1');
+          $citiess = $this->common->select_data_by_condition('cities', $contition_array, $data = 'city_name', $sortby = '', $orderby = '', $limit = '', $offset = '', $$join_str = array(), $groupby);
    
 
            foreach ($citiess as $key) {
