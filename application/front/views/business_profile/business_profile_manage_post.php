c1c249eb66cc12ad13a3a2751630f01edf122abe
ankit-aileensoul <ankit.aileensoul@gmail.com>
2017-06-19 20:11:34 -0400

    eod

--- application/front/views/business_profile/business_profile_manage_post.php
+++ application/front/views/business_profile/business_profile_manage_post.php
@@ -900,7 +900,7 @@ $(document).ready(function(){
                 </div>
 
                 <!-- popup start -->
-                <div class="col-md-7 col-sm-7 custom-right-business"  >
+                <div class="col-md-7 custom-right-business"  >
 
                     <div class="post-editor col-md-12">
                         <div class="main-text-area col-md-12">
@@ -1385,9 +1385,9 @@ $(document).ready(function(){
                                                     <!-- this div view all image start -->
                                                     <div>
                                                         <div id="responsive-manage_images_3-breakpoints" >
-                                                            <a href="<?php echo base_url('business_profile/postnewpage/' . $row['business_profile_post_id']) ?>"><img src="<?php echo base_url($this->config->item('bus_post_thumb_upload_path') . $businessmultiimage[3]['image_name']) ?>" style=" width: 100%; height: 100%;"> </a></div>
-
-                                                            <a href="<?php echo base_url('business_profile/postnewpage/' . $row['business_profile_post_id']) ?>">
+                                                            <a href="<?php echo base_url('business_profile/postnewpage/' . $row['business_profile_post_id']) ?>"><img src="<?php echo base_url($this->config->item('bus_post_thumb_upload_path') . $businessmultiimage[3]['image_name']) ?>" style=" width: 100%; height: 100%;"> </a>
+                                                        
+                                                          <a href="<?php echo base_url('business_profile/postnewpage/' . $row['business_profile_post_id']) ?>">
                                                         <div class="manage_images_view_more" >
 
 
@@ -1395,6 +1395,10 @@ $(document).ready(function(){
                                                          </span></div>
 
                                                         </a>
+                                                        
+                                                        </div>
+
+                                                          
 
                                                     </div>
                                                     <!-- this div view all image end -->
