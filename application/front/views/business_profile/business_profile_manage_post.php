dc180e1b0e17285f60206a22793de3cf628dcbaf
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-06-20 20:31:47 +0530

    edit post time description space remove

--- application/front/views/business_profile/business_profile_manage_post.php
+++ application/front/views/business_profile/business_profile_manage_post.php
@@ -24,17 +24,7 @@
 <script src="<?php echo base_url('dragdrop/themes/explorer/theme.js'); ?>"></script>
 
 
-<script type="text/javascript">
-//For Scroll page at perticular position js Start
-$(document).ready(function(){
- 
-  $(document).load().scrollTop(1000);
-     
-    //$('html,body').animate({scrollTop: 1000}, 100);
 
-});
-//For Scroll page at perticular position js End
-</script>
 
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/video.css'); ?>">
 <script src="<?php echo base_url('js/mediaelement-and-player.min.js'); ?>"></script>
@@ -4343,5 +4333,21 @@ jQuery(document).mouseup(function (e) {
 
   }
 </script>
+
+
+<!-- scroll page script start -->
+<script type="text/javascript">
+//For Scroll page at perticular position js Start
+$(document).ready(function(){
+ 
+//  $(document).load().scrollTop(1000);
+     
+    $('html,body').animate({scrollTop:330}, 100);
+
+});
+//For Scroll page at perticular position js End
+</script>
+
+<!-- scroll page script end -->
 
 
