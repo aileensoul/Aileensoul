62baab797ac4312cab1d74d9dde9d2f65954aa15
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-06-23 13:36:43 +0530

    no post found

--- application/front/views/business_profile/business_profile_post.php
+++ application/front/views/business_profile/business_profile_post.php
@@ -1,3 +1,4 @@
+
 <!-- start head -->
 <?php echo $head; ?>
 <!--post save success pop up style strat -->
@@ -507,7 +508,7 @@
                                                             <div class="profile-job-post-title-inside clearfix">
                                                                 <div class=" col-md-12 follow_left_box_main" id="<?php echo "fad" . $userlist['business_profile_id']; ?>">                   
                  <div class="post-design-pro-img_follow">
-                 
+
                          <?php if ($userlist['business_user_image']) { ?>
            <a href="<?php echo base_url('business_profile/business_profile_manage_post/' . $userlist['business_slug'] . ''); ?>" title="<?php echo ucwords($userlist['company_name']); ?>">
 
@@ -715,13 +716,14 @@
 
                         <?php
 //echo "<pre>"; print_r($businessprofiledata); die();
-                        if (count($businessprofiledata) > 0) {
-                            foreach ($businessprofiledata as $row) {
+
+                        if (count($businessprofiledatapost) > 0) { 
+                            foreach ($businessprofiledatapost as $row) {
                                 $userid = $this->session->userdata('aileenuser');
                                 $contition_array = array('business_profile_post_id' => $row['business_profile_post_id'], 'status' => '1');
                                 $businessdelete = $this->data['businessdelete'] = $this->common->select_data_by_condition('business_profile_post', $contition_array, $data = '*', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');
 
-     $contition_array = array('user_id' => $row['user_id'], 'status' => '1');
+                           $contition_array = array('user_id' => $row['user_id'], 'status' => '1');
                                 $businessdata = $this->common->select_data_by_condition('business_profile', $contition_array, $data = '*', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');
 
                                 $likeuserarray = explode(',', $businessdelete[0]['delete_post']);
@@ -1473,15 +1475,15 @@
                             }
                         } 
 
-
-     if(count($businessprofiledata) > 0){ 
-          if(count($count) == count($businessprofiledata)){  ?>
+ 
+     if(count($businessprofiledatapost) > 0){ 
+          if(count($count) == count($businessprofiledatapost)){  ?>
          <div class="contact-frnd-post bor_none">
          <div class="text-center rio">
             <h4 class="page-heading  product-listing" >No Post Found.</h4>
          </div>
          </div>
-         <?php } } else {
+         <?php } } else { 
                             ?>
                             <div class="contact-frnd-post bor_none">
                             <div class="text-center rio">
@@ -3384,16 +3386,16 @@
 <script type="text/javascript">
     function remove_post(abc)
     {
-        //alert(123);
+        alert(abc);
         $.ajax({
             type: 'POST',
-            url: '<?php echo base_url() . "business_profile/business_profile_deletepost" ?>',
+            url: '<?php echo base_url() . "business_profile/business_profile_deleteforpost" ?>',
             dataType: 'json',
             data: 'business_profile_post_id=' + abc,
             //alert(data);
-            success: function (data) {
+            success: function (data) { 
                 $('#' + 'removepost' + abc).remove();
-                 if(data.notcount == 0){
+                 if(data.notcount == 'count'){
                     $('.' + 'nofoundpost').html(data.notfound);
                     }
 
@@ -3416,7 +3418,7 @@
             success: function (data) {
                // $('#' + 'removepost' + abc).html(data);
                 $('#' + 'removepost' + abc).remove();
-                if(data.notcount == 0){
+                if(data.notcount == 'count'){
                     $('.' + 'nofoundpost').html(data.notfound);
                     }
 
