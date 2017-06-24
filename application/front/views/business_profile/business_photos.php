e36c59007869bb38c9fbb89be5dda811f94a7d3a
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-06-24 15:21:04 +0530

    add class

--- application/front/views/business_profile/business_photos.php
+++ application/front/views/business_profile/business_photos.php
@@ -284,7 +284,19 @@ start head -->
                         
 
                         <div class="profile-main-box-buis-menu">  
-                            <ul class="">
+                           
+                        <div class="profile-main-box-buis-menu ml0">  
+                                   <?php 
+               $userid = $this->session->userdata('aileenuser');
+               if($businessdata1[0]['user_id'] == $userid){
+               
+               ?>     
+             <ul class="current-user bpro-fw6">
+                   
+                   <?php }else{?>
+                 <ul class="bpro-fw">
+                   <?php } ?>  
+
 
 
 
