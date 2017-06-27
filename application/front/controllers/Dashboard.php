d5a3955e05f32824da42e7cb01e212956eb50add
DESKTOP-SHE2KEK\Aileensoul <ankit.aileensoul@gmail.com>
2017-06-27 11:16:03 -0400

    updated work

--- application/front/controllers/Dashboard.php
+++ application/front/controllers/Dashboard.php
@@ -52,12 +52,19 @@ class Dashboard extends MY_Controller {
               }
              */
             
+//            echo '<pre>';
+//            print_r($_FILES);
+//            exit;
+            
             $user_image = '';
             $user['upload_path'] = $this->config->item('user_main_upload_path');
             $user['allowed_types'] = $this->config->item('user_main_allowed_types');
             $user['max_size'] = $this->config->item('user_main_max_size');
             $user['max_width'] = $this->config->item('user_main_max_width');
             $user['max_height'] = $this->config->item('user_main_max_height');
+            $user['remove_spaces'] = true;
+            $user['quality'] = "50%";
+            $user['encrypt_name'] = TRUE;
             $this->load->library('upload');
             $this->upload->initialize($user);
             //Uploading Image
