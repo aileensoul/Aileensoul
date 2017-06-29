507ab9ea395228f1793ef02e4bdcfe675004e256
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-06-29 12:39:14 +0530

    main

--- application/front/controllers/Contact_us.php
+++ application/front/controllers/Contact_us.php
@@ -32,12 +32,15 @@ class Contact_us extends CI_Controller {
     public function contact_us_insert() 
      { 
    
-          $name = $_POST['name'];
-          $email = $_POST['email'];
-          $subject = $_POST['subject'];
-          $message = $_POST['message'];
+          $name = $_POST['contact_name'];
+          $contactlast_name = $_POST['contactlast_name'];
+          $email = $_POST['contact_email'];
+          $subject = $_POST['contact_subject'];
+          $message = $_POST['contact_message'];
 
         $this->form_validation->set_rules('contact_name', 'contact name', 'required');
+        $this->form_validation->set_rules('contactlast_name', 'contact name', 'required');
+
         $this->form_validation->set_rules('contact_email', 'contact email', 'required|valid_email');
         $this->form_validation->set_rules('contact_subject','contact subject', 'required');
         $this->form_validation->set_rules('contact_message','contact message', 'required');
@@ -46,18 +49,17 @@ class Contact_us extends CI_Controller {
             $data = array(
 
                 'contact_name' =>  $name,
+                'contact_lastname' => $contactlast_name,
                 'contact_email' => $email,
                 'contact_subject' => $subject,
                 'contact_message' => $message,
                 ); 
                 //echo"<pre>";print_r($data);die();
               $insert_id =   $this->common->insert_data_getid($data,'contact_us'); 
-               //  if($insert_id)
-               //  { 
-               //         $this->session->set_flashdata('success', 'successfully');
-               //        redirect('main');
-                  
-               //  }
+                if($insert_id)
+                { 
+                      echo "ok";
+                }
                // else
                //  {
                //          $this->session->flashdata('error','Sorry!! Your data not inserted');
--- application/front/views/contact/contact_us.php
+++ application/front/views/contact/contact_us.php
@@ -32,31 +32,36 @@
         <div class="title">
           <h1>Contact us</h1>
         </div>
+
+
+        <div id="contactsucc"></div>
+
+
         <div class="inner-form">
           <div class="login">
             
-            <form role="form">
+            <form role="form" name="contact_form" id="contact_form" method="post">
                 <div class="row">
                   <div class="col-sm-6 col-md-6">
                     <div class="form-group">
-                      <input type="text" name="first_name" id="first_name" class="form-control input-sm required" placeholder="First Name*">
+                      <input type="text" name="contact_name" id="contact_name" class="form-control input-sm required" placeholder="First Name*">
                     </div>
                   </div>
                   <div class="col-sm-6 col-md-6">
                     <div class="form-group">
-                      <input type="text" name="last_name" id="last_name" class="form-control input-sm" placeholder="Last Name*">
+                      <input type="text" name="contactlast_name" id="contactlast_name" class="form-control input-sm" placeholder="Last Name*">
                     </div>
                   </div>
                 </div>
 
                 <div class="form-group">
-                  <input type="email" name="email" id="email" class="form-control input-sm" placeholder="Email Address*">
+                  <input type="email" name="contact_email" id="contact_email" class="form-control input-sm" placeholder="Email Address*">
                 </div>
               <div class="form-group">
-                  <input type="text" name="subject" id="subject" class="form-control input-sm" placeholder="Subject*">
+                  <input type="text" name="contact_subject" id="contact_subject" class="form-control input-sm" placeholder="Subject*">
                 </div>
               <div class="form-group">
-                <textarea type="text" class="form-control" placeholder="Message*"></textarea>
+                <textarea type="text" id="contact_message" name="contact_message" class="form-control" placeholder="Message*"></textarea>
                   
                 </div>
               
@@ -65,7 +70,7 @@
                 <span class="red">*</span>All fields are mendatory
               </p>
                 <p>
-                <a href="#" class="btn1">Submit</a>
+                <button class="btn1">Submit</button>
               </p>
               </form>
             
@@ -262,6 +267,114 @@
   
   });
 </script>
+
+<script type="text/javascript" src="<?php echo base_url() ?>js/jquery.validate.min.js"></script>
+<!-- validation for edit email formate form strat -->
+
+<script>
+                            $(document).ready(function () { 
+                                $("#contact_form").validate({ 
+                                    rules: {
+                                        contact_name: {
+                                            required: true,
+                                        },
+                                        contactlast_name: {
+                                            required: true,
+                                        },
+                                        contact_email: {
+                                            required: true,
+                                        },
+                                        contact_subject: {
+                                            required: true,
+                                        },
+                                        contact_message: {
+                                            required: true,
+                                        }
+                                        
+                                    },
+
+
+                                    messages:
+                                            {
+                                                contact_name: {
+                                                    required: "Please enter first name",
+                                                },
+                                                contactlast_name: {
+                                                    required: "Please enter last name",
+                                                },
+                                                contact_email: {
+                                                    required: "Please enter email address",
+                                                    
+                                                },
+                                                contact_subject: {
+                                                    required: "Please enter subject",
+                                                },
+                                              
+                                                contact_message: {
+                                                    required: "Please enter your message",
+                                                }
+                                               
+                                            },
+                                    submitHandler: submitRegisterForm
+                                });
+                                /* register submit */
+                                function submitRegisterForm()
+                                {
+                                    var contact_name = $("#contact_name").val();
+                                    var contactlast_name = $("#contactlast_name").val();
+                                    var contact_email = $("#contact_email").val();
+                                    var contact_subject = $("#contact_subject").val();
+                                    var contact_message = $("#contact_message").val();
+                                    
+                                    
+                                    var post_data = {
+                                        'contact_name': contact_name,
+                                        'contactlast_name': contactlast_name,
+                                        'contact_email': contact_email,
+                                        'contact_subject': contact_subject,
+                                        'contact_message': contact_message,
+                                        
+                                        '<?php echo $this->security->get_csrf_token_name(); ?>': '<?php echo $this->security->get_csrf_hash(); ?>'
+                                    }
+                                    $.ajax({
+                                        type: 'POST',
+                                        url: '<?php echo base_url() ?>contact_us/contact_us_insert',
+                                        data: post_data,
+                                        beforeSend: function ()
+                                        {
+                                            $("#register_error").fadeOut();
+                                            $("#btn-register").html('Sign Up ...');
+                                        },
+                                        success: function (response)
+                                        {
+                                            if (response == "ok") {
+
+                                              $("#contact_name").val('');
+                                              $("#contactlast_name").val('');
+                                              $("#contact_email").val('');
+                                              $("#contact_subject").val('');
+                                              $("#contact_message").val('');
+                                              
+                                                 $("#contactsucc").fadeIn(1000, function () {
+                                                    $("#contactsucc").html('<div class="alert alert-danger"> <i class="fa fa-info-circle" aria-hidden="true"></i> &nbsp; ' + 'Your message send successfully' + ' !</div>');
+                                                    $("#btn-register").html('Sign Up');
+                                                });
+                                               // setTimeout(' window.location.href = "<?php //echo base_url() ?>dashboard"; ', 4000);
+                                            }
+                                            else {
+                                                $("#contactsucc").fadeIn(1000, function () {
+                                                    $("#contactsucc").html('<div class="alert alert-danger"> <i class="fa fa-info-circle" aria-hidden="true"></i> &nbsp; ' +'your conatct not send successfully' + ' !</div>');
+                                                    $("#btn-register").html('Sign Up');
+                                                });
+                                            }
+                                        }
+                                    });
+                                    return false;
+                                }
+                               });
+
+</script>
+
 
 </body>
 </html>
