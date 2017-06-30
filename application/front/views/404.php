05eb64d9d7efc7b830a6a4abbaaa30764eea5f1d
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-06-30 16:17:41 +0530

    main

--- application/front/views/404.php
+++ application/front/views/404.php
@@ -1,7 +1,12 @@
 <?php echo $head; ?>
 
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/gyc.css'); ?>">
+
+<?php 
+   $userid = $this->session->userdata('aileenuser');
+if($userid){?>
 <?php echo $header; ?>
+<?php }?>
 
 <link rel="stylesheet" href="<?php echo base_url() ?>css/bootstrap.min.css" />
 <body   class="page-container-bg-solid page-boxed">
