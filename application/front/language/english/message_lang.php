a1586f0c5ce7c110dfdb3c68722f96d0d668fc9b
DESKTOP-K4PBRSP\Aileensoul <ankit.aileensoull@gmail.com>
2017-07-04 10:54:31 +0530

    Updated Work

--- application/front/language/english/message_lang.php
+++ application/front/language/english/message_lang.php
@@ -14,6 +14,8 @@ $lang["post_your_product"] = "Post Your Product...";
 
 $lang["bus_reg_title"] = "You are making your Business Profile.";
 $lang["bus_reg_edit_title"] = "You are updating your Business Profile.";
+$lang["business_information"] = "Business Information";
+$lang["business_information"] = "Business Information";
 
 /* BUSINESS INFORMATION */
 
