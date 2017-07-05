65b75276643f032a68c2ee93db130bef336f6f89
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-07-05 15:26:52 +0530

    add css

--- application/front/views/business_profile/business_photos.php
+++ application/front/views/business_profile/business_photos.php
@@ -421,7 +421,7 @@ start head -->
                 </div>
             </div>
         </div>
-      <div class="container">
+      <div class="">
          <div class="user-midd-section"  >
             <div  class="col-sm-12 border_tag padding_low_data padding_les" >
                <div class="padding_les main_art" >
