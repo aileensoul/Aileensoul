b1a578ce0f73f7a13731db88c9d5fa875b708f73
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-07-06 12:50:46 +0530

    add <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />

--- application/front/views/main.php
+++ application/front/views/main.php
@@ -3,6 +3,7 @@
 <head>
   <title>Grow Business Network|Hiring|Search Jobs|Freelance Work|It's Free|Aileensoul</title>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/common-style.css">
   <link rel="stylesheet" href="css/style-main.css">
