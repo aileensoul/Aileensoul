7b451ada1c74e068677100bab0a49ccbf82a1986
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-07-06 15:54:40 +0530

    add

--- application/front/views/header.php
+++ application/front/views/header.php
@@ -498,7 +498,7 @@ if($userid){?>
 
                                     <div id="InboxContainer">
                                         <div id="InboxBody" class="Inbox">
-                                        
+                                           <div id="notificationTitle">Messages</div>
 
                                             <div id="notificationsmsgBody" class="notificationsmsg">
 
