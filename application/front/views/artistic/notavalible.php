3df50b8ae6f3ec59d75f101d47fd432bf7e492ab
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-08-01 11:07:50 +0530

    ADD CSS

--- application/front/views/artistic/notavalible.php
+++ application/front/views/artistic/notavalible.php
@@ -12,8 +12,19 @@
  <?php echo $header; ?>
 
  <?php echo $art_header2_border; ?>
- 	<div class="" style="padding-top: 100px; text-align: center;">
-      <div>Sorry, this content isn't available at the moment</div>
+ <div class="user-midd-section" id="paddingtop_fixed">    <div class="container" id="paddingtop_fixed">
+    	<div class="row">
+    		<div class="col-md-12">
+    			<div class="text_center">
+    				<div class="sory_image">
+    					<img src="<?php echo base_url('img/sorry_img.png'); ?>" >
+    				</div>
+    				<div class="not_founde_head">Sorry !</div>
+    				<div class="not_founde_head2">we coundn’t find any matches with your input.</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
     </div>
       </body>
       </html>
