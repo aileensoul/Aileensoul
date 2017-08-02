1f531194d4433524e4bc580c9ba005dc5fd7b7a5
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-08-02 13:31:12 +0530

    C:\xampp\htdocs\aileensoul\application\front\views\business_profile\business_header2_border.php

--- application/front/views/business_profile/business_header2_border.php
+++ application/front/views/business_profile/business_header2_border.php
@@ -462,7 +462,7 @@
         } else
         {
             $('#message_count').html(msg);
-            $('#message_count').css({"background-color": "#FF4500" , "height": "16px" ,"width": "16px" , "padding" : "3px 4px"});
+            $('#message_count').css({"background-color": "#FF4500" , "height": "17px" ,"width": "17px" , "padding" : "3px 4px"});
             $('#InboxLink').addClass('msg_notification_available');
             //alert("welcome");
         }
