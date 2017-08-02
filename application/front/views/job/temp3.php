c40241ba420bbee556aaeda54120b122082aea31
khyati@123456 <khyati.aileensoul@gmail.com>
2017-08-02 13:26:38 +0530

    job controller and job_skill

--- application/front/views/job/temp3.php
+++ application/front/views/job/temp3.php
@@ -13,7 +13,147 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/1.10.3.jquery-ui.css'); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/timeline.css'); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/test.css'); ?>">
+	<style type="text/css">
 
+/* Layout helpers
+----------------------------------*/
+.ui-helper-hidden {
+	display: none;
+}
+.ui-helper-hidden-accessible {
+	border: 0;
+	clip: rect(0 0 0 0);
+	height: 1px;
+	margin: -1px;
+	overflow: hidden;
+	padding: 0;
+	position: absolute;
+	width: 1px;
+}
+
+.ui-front {
+	z-index: 100;
+}
+
+
+
+/* Misc visuals
+----------------------------------*/
+
+/* Overlays */
+.ui-widget-overlay {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+}
+
+.ui-autocomplete {
+	position: absolute;
+	top: 0;
+	left: 0;
+	cursor: default;
+}
+
+.ui-menu {
+	list-style: none;
+	padding: 0;
+	margin: 0;
+	display: block;
+	outline: none;
+}
+.ui-menu .ui-menu {
+	position: absolute;
+}
+.ui-menu .ui-menu-item {
+	position: relative;
+	margin: 0;
+	padding: 3px 1em 3px .4em;
+	cursor: pointer;
+	min-height: 0; /* support: IE7 */
+	/* support: IE10, see #8844 */
+	list-style-image: url("data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7");
+}
+.ui-menu .ui-menu-divider {
+	margin: 5px 0;
+	height: 0;
+	font-size: 0;
+	line-height: 0;
+	border-width: 1px 0 0 0;
+}
+.ui-menu .ui-state-focus,
+.ui-menu .ui-state-active {
+	margin: -1px;
+}
+
+/* Component containers
+----------------------------------*/
+.ui-widget {
+	font-family: Verdana,Arial,sans-serif;
+	font-size: 1.1em;
+}
+.ui-widget .ui-widget {
+	font-size: 1em;
+}
+.ui-widget input,
+.ui-widget select,
+.ui-widget textarea,
+.ui-widget button {
+	font-family: Verdana,Arial,sans-serif;
+	font-size: 1em;
+}
+.ui-widget-content {
+	border: 1px solid #aaaaaa;
+	background: #ffffff url("images/ui-bg_flat_75_ffffff_40x100.png") 50% 50% repeat-x;
+	color: #222222;
+}
+.ui-widget-content a {
+	color: #222222;
+}
+.ui-widget-header {
+	border: 1px solid #aaaaaa;
+	background: #cccccc url("images/ui-bg_highlight-soft_75_cccccc_1x100.png") 50% 50% repeat-x;
+	color: #222222;
+	font-weight: bold;
+}
+.ui-widget-header a {
+	color: #222222;
+}
+
+/* Interaction states
+----------------------------------*/
+.ui-state-default,
+.ui-widget-content .ui-state-default,
+.ui-widget-header .ui-state-default {
+	border: 1px solid #d3d3d3;
+	background: #e6e6e6 url("images/ui-bg_glass_75_e6e6e6_1x400.png") 50% 50% repeat-x;
+	font-weight: normal;
+	color: #555555;
+}
+
+.ui-state-hover,
+.ui-widget-content .ui-state-hover,
+.ui-widget-header .ui-state-hover,
+.ui-state-focus,
+.ui-widget-content .ui-state-focus,
+.ui-widget-header .ui-state-focus {
+	border: 1px solid #999999;
+	background: #dadada url("images/ui-bg_glass_75_dadada_1x400.png") 50% 50% repeat-x;
+	font-weight: normal;
+	color: #212121;
+}
+
+.ui-state-active,
+.ui-widget-content .ui-state-active,
+.ui-widget-header .ui-state-active {
+	border: 1px solid #aaaaaa;
+	background: #ffffff url("images/ui-bg_glass_65_ffffff_1x400.png") 50% 50% repeat-x;
+	font-weight: normal;
+	color: #212121;
+}
+
+	</style>
 <!-- start header -->
 <?php echo $header; ?>
 
@@ -49,13 +189,13 @@
                                 <fieldset class="fresher_radio" >
                                     <label>Fresher ? <font  color="red">*</font> : </label>
                                     <div class="main_raio">
-                                        <input type="radio" id="test1" name="fresher" id="fresher">
+                                        <input type="radio" value="Fresher" id="test1" name="fresher" id="fresher">
                                         <label for="test1">Yes</label>
                                     </div>
 
 
                                     <div class="main_raio">
-                                        <input type="radio" id="test2" name="fresher" id="fresher">
+                                        <input type="radio" value="Experience" id="test2" name="fresher" id="fresher">
                                         <label for="test2">No</label>
                                     </div>
 
@@ -66,25 +206,10 @@
                                      <input type="search" id="job_title" name="job_title" value="" placeholder="Ex:- Sr. Engineer, Jr. Engineer, Software Developer, Account Manager">
                                 </fieldset>
 
-                                <fieldset class="full-width fresher_select main_select_data" >
-                                    <label>skill <font  color="red">*</font> :</label>
-                                     <select name="skills1[]"  class="keyskil skil" id ="skills1" tabindex="1" autofocus  multiple="multiple" style="width:100%;" placeholder='Enter Skill'>
-                                                         <option></option>
-                                   <?php foreach ($skill as $ski) { ?>
-                                            <option value="<?php echo $ski['skill_id']; ?>"><?php echo $ski['skill']; ?></option>
-<?php } ?>
-                                     </select>
-
-                                </fieldset>
-                                <fieldset class="full-width pt0 pb0">
-                                    <label id="other_skill">If you have not found your skill <span class="other_skill_clik"> click Here </span>to add your skill.</label>
-
-                                    <div id="other_skill_data" style="display: none;">
-                                        <label>Other Skill :</label>
-                                        <input type="text" name="other_skill" class="keyskil" id="other_skill">
-                                    </div>
-
-                                </fieldset>
+<fieldset class="full-width fresher_select main_select_data" >
+    <label for="skills"> Skills: </label>
+    <input id="skills2" name="skills" id="cities" size="90">
+  </fieldset>
 
 
                                 <fieldset class="full-width main_select_data">
@@ -96,16 +221,16 @@
 <?php } ?>
                                     </select>
                                 </fieldset>
-                                 <fieldset class="full-width fresher_select main_select_data" >
-                                    <label>city <font  color="red">*</font> :</label>
-                                     <select name="city1[]"  class="city" id ="city1" tabindex="1" autofocus  multiple="multiple" style="width:100%;" placeholder='Enter Skill'>
-                                                         <option></option>
-                                   <?php foreach ($citydata as $city) { ?>
-                                            <option value="<?php echo $city['city_id']; ?>"><?php echo $city['city_name']; ?></option>
-<?php } ?>
-                                     </select>
 
-                                </fieldset>
+                                
+                             <fieldset class="full-width fresher_select main_select_data" >
+    <label for="cities">Preferred Cites: </label>
+    <input id="cities2" name="cities" id="cities" size="90">
+  </fieldset>
+
+ 
+
+
                                 <fieldset class=" full-width">
 
                                     <div class="job_reg">
@@ -129,13 +254,9 @@
     
     
     <!-- END CONTAINER -->
-    <script type="text/javascript">
-        $('#other_skill').click(function () {
-            $('#other_skill_data').toggle()
-        })
-    </script>
-    <script src="<?php echo base_url('js/demo/jquery-1.9.1.js'); ?>"></script>
-<script src="<?php echo base_url('js/demo/jquery-ui-1.9.1.js'); ?>"></script>
+    
+    <script src="<?php //echo base_url('js/demo/jquery-1.9.1.js'); ?>"></script>
+<script src="<?php// echo base_url('js/demo/jquery-ui-1.9.1.js'); ?>"></script>
      <script type="text/javascript" src="<?php echo base_url('js/jquery.validate.min.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('js/jquery.validate.js'); ?>"></script>
 
@@ -143,73 +264,13 @@
    
     <script type="text/javascript" src="<?php echo base_url('js/additional-methods1.15.0.min.js'); ?>"></script>
   <script src="<?php echo base_url('js/bootstrap.min.js'); ?>"></script>
-    
-    <script type="text/javascript">
-//select2 autocomplete start for skill
-    var complex = <?php echo json_encode($selectdata); ?>;
-   // $('#skils').select2().select2('val', complex)
-
-    if(complex != null && complex != '')
-    { 
-        
-         $(".skil").select2({
-         placeholder: "Select a Language",
-         }).select2('val', complex);
-    }
-//    if(complex != '')
-//    { 
-//        alert(789);
-//         $(".skil").select2({
-//         placeholder: "Select a Language",
-//         }).select2('val', complex);
-//    }
-   if(complex == '' || complex == null)
-    {
-       
-         $(".skil").select2({
-         placeholder: "Select a Language",
  
-        });
-    }
-//select2 autocomplete End for skill
-</script>
-
-<script type="text/javascript">
-//select2 autocomplete start for skill
-   // var complex = <?php echo json_encode($selectdata); ?>;
-   // $('#skils').select2().select2('val', complex)
-
-  //  if(complex != null && complex != '')
-  //  { 
-        
-         $(".city").select2({
-         placeholder: "Select a Language",
-         }).select2('val', complex);
-   // }
-//    if(complex != '')
-//    { 
-//        alert(789);
-//         $(".skil").select2({
-//         placeholder: "Select a Language",
-//         }).select2('val', complex);
-//    }
-//   if(complex == '' || complex == null)
-//    {
-//       
-//         $(".skil").select2({
-//         placeholder: "Select a Language",
-// 
-//        });
-//    }
-//select2 autocomplete End for skill
-</script>
-
 <script>
     // job title script start
    var jobdata = <?php echo json_encode($jobtitle); ?>;
    
    $(function () {
-       // alert('hi');
+    
        $("#job_title").autocomplete({
            source: function (request, response) {
                var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(request.term), "i");
@@ -235,34 +296,7 @@
 </script>
 
 <script>
-//    // job title script start
-//   var data = <?php echo json_encode($city_data); ?>;
-//  
-//   $(function () {
-//       // alert('hi');
-//       $("#cities").autocomplete({
-//           source: function (request, response) {
-//               var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(request.term), "i");
-//               response($.grep(data, function (item) {
-//                   return matcher.test(item.label);
-//               }));
-//           },
-//           minLength: 1,
-//           select: function (event, ui) {
-//               event.preventDefault();
-//               $("#cities").val(ui.item.label);
-//               $("#selected-tag").val(ui.item.label);
-//               // window.location.href = ui.item.value;
-//           }
-//           ,
-//           focus: function (event, ui) {
-//               event.preventDefault();
-//               $("#cities").val(ui.item.label);
-//           }
-//       });
-//   });
-   
-   
+
    $.validator.addMethod("regx", function(value, element, regexpr) {          
     //return value == '' || value.trim().length != 0; 
      if(!value) 
@@ -311,18 +345,18 @@
 
                     required: true,
                     email: true,
-                    lowercase: /^[0-9a-z\s\r\n@!#\$\^%&*()+=_\-\[\]\\\';,\.\/\{\}\|\":<>\?]+$/,
-                    remote: {
-                        url: "<?php echo site_url() . 'job/check_email' ?>",
-                        type: "post",
-                        data: {
-                            email: function () {
-
-                                return $("#email").val();
-                            },
-                            '<?php echo $this->security->get_csrf_token_name(); ?>': '<?php echo $this->security->get_csrf_hash(); ?>',
-                        },
-                    },
+                   // lowercase: /^[0-9a-z\s\r\n@!#\$\^%&*()+=_\-\[\]\\\';,\.\/\{\}\|\":<>\?]+$/,
+//                    remote: {
+//                        url: "<?php echo site_url() . 'job/check_email' ?>",
+//                        type: "post",
+//                        data: {
+//                            email: function () {
+//
+//                                return $("#email").val();
+//                            },
+//                            '<?php echo $this->security->get_csrf_token_name(); ?>': '<?php echo $this->security->get_csrf_hash(); ?>',
+//                        },
+//                    },
                 },
 
                 fresher: {
@@ -431,6 +465,117 @@
         });
     
 </script>
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<!--new script for cities start-->
+ <script>
+    $(function() {
+        function split( val ) {
+            return val.split( /,\s*/ );
+        }
+        function extractLast( term ) {
+            return split( term ).pop();
+        }
+        
+        $( "#cities2" ).bind( "keydown", function( event ) {
+            if ( event.keyCode === $.ui.keyCode.TAB &&
+                $( this ).autocomplete( "instance" ).menu.active ) {
+                event.preventDefault();
+            }
+        })
+        .autocomplete({
+            minLength: 2,
+            source: function( request, response ) { 
+                // delegate back to autocomplete, but extract the last term
+                $.getJSON("<?php echo base_url();?>general/get_location", { term : extractLast( request.term )},response);
+            },
+            focus: function() {
+                // prevent value inserted on focus
+                return false;
+            },
+            select: function( event, ui ) {
+                alert(ui.item.value);
+                alert(ui.item.id);
+                alert();
+                var terms = split( this.value );
+                if(terms.length <= 3) {
+                    // remove the current input
+                    terms.pop();
+                    // add the selected item
+                    terms.push( ui.item.value );
+                    // add placeholder to get the comma-and-space at the end
+                    terms.push( "" );
+                    this.value = terms.join( ", " );
+                    return false;
+                }else{
+                    var last = terms.pop();
+                    $(this).val(this.value.substr(0, this.value.length - last.length - 2)); // removes text from input
+                    $(this).effect("highlight", {}, 1000);
+                    $(this).attr("style","border: solid 1px red;");
+                    return false;
+                }
+            }
 
+  
+ 
+        });
+    });
+</script>
+<!--new script for cities end-->
+<!--new script for skill start-->
+ <script>
+    $(function() {
+        function split( val ) {
+            return val.split( /,\s*/ );
+        }
+        function extractLast( term ) { 
+            return split( term ).pop();
+        }
+        
+        $( "#skills2" ).bind( "keydown", function( event ) {
+            if ( event.keyCode === $.ui.keyCode.TAB &&
+                $( this ).autocomplete( "instance" ).menu.active ) {
+                event.preventDefault();
+            }
+        })
+        .autocomplete({
+            minLength: 2,
+            source: function( request, response ) { 
+                // delegate back to autocomplete, but extract the last term
+                $.getJSON("<?php echo base_url();?>general/get_skill", { term : extractLast( request.term )},response);
+            },
+            focus: function() {
+                // prevent value inserted on focus
+                return false;
+            },
+            select: function( event, ui ) {
+                alert(ui.item.value);
+                alert(ui.item.id);
+                alert();
+                var terms = split( this.value );
+                if(terms.length <= 3) {
+                    // remove the current input
+                    terms.pop();
+                    // add the selected item
+                    terms.push( ui.item.value );
+                    // add placeholder to get the comma-and-space at the end
+                    terms.push( "" );
+                    this.value = terms.join( ", " );
+                    return false;
+                }else{
+                    var last = terms.pop();
+                    $(this).val(this.value.substr(0, this.value.length - last.length - 2)); // removes text from input
+                    $(this).effect("highlight", {}, 1000);
+                    $(this).attr("style","border: solid 1px red;");
+                    return false;
+                }
+            }
+
+  
+ 
+        });
+    });
+</script>
+<!--new script for skill end-->
 </body>
 </html>
