7bf02c7a56557e873d0547a68d8f1e8905003487
khyati@123456 <khyati.aileensoul@gmail.com>
2017-08-03 16:53:51 +0530

    merge

--- application/front/controllers/Profile.php
+++ application/front/controllers/Profile.php
@@ -20,7 +20,11 @@ class Profile extends CI_Controller {
       $userid =  $this->session->userdata('aileenuser');
       $this->data['userdata'] =  $this->common->select_data_by_id('user', 'user_id', $userid, $data = '*', $join_str = array());
 
-    //  echo '<pre>';      print_r($this->data['userdata']); die();
+      
+      $this->data['usry'] = date('Y',strtotime($this->data['userdata'][0][user_dob]));
+      $this->data['usrm'] = date('m',strtotime($this->data['userdata'][0][user_dob]));
+      $this->data['usrd'] = date('d',strtotime($this->data['userdata'][0][user_dob]));
+  
         $this->load->view('profile/profile', $this->data);
     }
 
