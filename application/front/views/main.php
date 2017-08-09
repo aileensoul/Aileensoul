403e70ad6aa7f7a933a07120ea70c371d34b2eb5
DESKTOP-FNQCPB7\A <ankit.aileensoul@gmail.com>
2017-08-09 12:23:36 +0530

    SEO Changes

--- application/front/views/main.php
+++ application/front/views/main.php
@@ -43,10 +43,7 @@
         <link rel="stylesheet" href="css/style_harshad.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    </head>
-
-    <body>
-<script type="application/ld+json">
+        <script type="application/ld+json">
             {
             "@context": "http://schema.org",
             "@type": "Organization",
@@ -61,6 +58,9 @@
             ]
             }
         </script>
+    </head>
+
+    <body>
         <div class="main-login">
             <header>
                 <div class="container">
