936abc8441339a83ff971fc2ed8a4d7c93fe5e0a
aarati-aileensoul <aarati.aileensoul@gmail.com>
2017-08-10 18:49:00 +0530

    aarati changes

--- application/front/views/job/job_printpreview.php
+++ application/front/views/job/job_printpreview.php
@@ -340,14 +340,14 @@ if ($returnpage == 'recruiter') {
                    
             </div>
             <div class="col-md-7 col-sm-12 mob-clear">
-				<div class="mob-progressbar">
+				<div class="mob-progressbar <?php if($count_profile == 100){?>temp<?php } ?>">
 					<p>Please fill up your entire profile to get better job options and so that recruiter can find you easily.</p>
 					<p class="mob-edit-pro">
 						<a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit Profile</a>
 					</p>
 					<div class="progress skill-bar ">
-						<div class="progress-bar progress-bar-custom" role="progressbar" aria-valuenow="<?php echo round($count_profile);?>" aria-valuemin="0" aria-valuemax="100">
-							<span class="skill"><i class="val"><?php echo $count_profile;?>%</i></span>
+						<div class="progress-bar progress-bar-custom" role="progressbar" aria-valuenow="<?php echo($count_profile);?>" aria-valuemin="0" aria-valuemax="100">
+							<span class="skill"><i class="val"><?php echo(round($count_profile));?>%</i></span>
 						</div>
 					</div>
 					
