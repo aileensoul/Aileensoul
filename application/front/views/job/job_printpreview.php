7d458312fcd083fddef5ee8b007348fd6b499c2e
aarati-aileensoul <aarati.aileensoul@gmail.com>
2017-08-10 18:53:16 +0530

    aarati changes

--- application/front/views/job/job_printpreview.php
+++ application/front/views/job/job_printpreview.php
@@ -343,7 +343,7 @@ if ($returnpage == 'recruiter') {
 				<div class="mob-progressbar <?php if($count_profile == 100){?>temp<?php } ?>">
 					<p>Please fill up your entire profile to get better job options and so that recruiter can find you easily.</p>
 					<p class="mob-edit-pro">
-						<a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit Profile</a>
+						<a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> <?php if($count_profile == 100){?> Successfully Completed <?php }else{ ?>Edit Profile<?php } ?></a>
 					</p>
 					<div class="progress skill-bar ">
 						<div class="progress-bar progress-bar-custom" role="progressbar" aria-valuenow="<?php echo($count_profile);?>" aria-valuemin="0" aria-valuemax="100">
