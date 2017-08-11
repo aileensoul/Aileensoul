cc583b8577c5f163106de0ba5a9803ec4ee46dc4
DESKTOP-FNQCPB7\A <ankit.aileensoul@gmail.com>
2017-08-11 15:13:00 +0530

    cover page changes

--- application/front/views/dashboard/cover.php
+++ application/front/views/dashboard/cover.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" class="custom-c">
 <head>
-  <title>aileensoul</title>
+  <title>Aileensoul</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
   <link rel="stylesheet" href="css/common-style.css">
