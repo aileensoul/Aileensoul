ab2c9755877c0c177545a36b25b22fd5c50553d5
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-08-23 19:42:06 +0530

    art bugs

--- application/front/views/artistic/art_manage_post.php
+++ application/front/views/artistic/art_manage_post.php
@@ -981,14 +981,14 @@ if (!file_exists($this->config->item('art_profile_thumb_upload_path') . $userima
                         <div class="main-text-area" >
                             <div class="popup-img-in_art"> 
 
-                            <?php if($artisticdata[0]['art_user_image']){?>
+                            <?php if($artdata[0]['art_user_image']){?>
 
                              <?php 
 
-if (!file_exists($this->config->item('art_profile_thumb_upload_path') . $artisticdata[0]['art_user_image'])) {
-                                                                $a = $artisticdata[0]['art_name'];
+if (!file_exists($this->config->item('art_profile_thumb_upload_path') . $artdata[0]['art_user_image'])) {
+                                                                $a = $artdata[0]['art_name'];
                                                                 $acr = substr($a, 0, 1);
-                                                                $b = $artisticdata[0]['art_lastname'];
+                                                                $b = $artdata[0]['art_lastname'];
                                                                 $bcr = substr($b, 0, 1);
                                                                 ?>
                                                                 <div class="post-img-div">
@@ -997,16 +997,16 @@ if (!file_exists($this->config->item('art_profile_thumb_upload_path') . $artisti
                                                                 <?php
                                                             } else { ?>
 
-                            <img  src="<?php echo base_url($this->config->item('art_profile_thumb_upload_path') . $artisticdata[0]['art_user_image']); ?>"  alt="">
+                            <img  src="<?php echo base_url($this->config->item('art_profile_thumb_upload_path') . $artdata[0]['art_user_image']); ?>"  alt="">
 
                             <?php }?>
 
                             <?php }else{?>
 
                             <?php 
-                         $a = $artisticdata[0]['art_name'];
+                         $a = $artdata[0]['art_name'];
                                                                 $acr = substr($a, 0, 1);
-                                                                $b = $artisticdata[0]['art_lastname'];
+                                                                $b = $artdata[0]['art_lastname'];
                                                                 $bcr = substr($b, 0, 1);?>
 
                             <div class="post-img-div">
