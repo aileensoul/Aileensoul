27248a86ddb7ea8bf5f6de3fe2b1cf0776d390e3
aarati-aileensoul <aarati.aileensoul@gmail.com>
2017-09-16 12:36:52 +0530

    aarati changes

--- admin/application/admin/views/leftmenu.php
+++ admin/application/admin/views/leftmenu.php
@@ -82,6 +82,18 @@
                 </ul>
             </li>
             <!--End Freelancer Hire Management-->
+
+            <!--Start Business Management-->
+            <li <?php if ($this->uri->segment(1) == 'business' || $this->uri->segment(1) == '') { ?> class="active treeview" <?php } else { ?> class="treeview"   <?php } ?>>
+                <a href="#">
+                    <i class="fa fa-briefcase margin-r-5"></i> <span>Business Management</span>
+                    <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="<?php echo base_url('business/user'); ?>"><i class="fa fa-users"></i>List Business User</a></li>
+                </ul>
+            </li>
+            <!--End Business Management-->
             
              <!--Start search keyword  Management-->
             <li <?php if ($this->uri->segment(1) == 'search_keyword' || $this->uri->segment(1) == '') { ?> class="active treeview" <?php } else { ?> class="treeview"   <?php } ?>>
